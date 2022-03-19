6f323b8a0b6700bbc9f53f37f30c2961f2baadc0
Chava ALV <msalvarado.lopez@gmail.com>
2022-03-19 03:35:01 -0600

    commit Marzo 19 2022, DOS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatalogoSwiperComponent } from './catalogo-swiper/catalogo-swiper.component';
 import { MenuHeaderComponent } from './menu-header/menu-header.component';
+import { MenuFooterComponent } from './menu-footer/menu-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CatalogoSwiperComponent,
-    MenuHeaderComponent
+    MenuHeaderComponent,
+    MenuFooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/menu-footer/menu-footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-footer',
+  templateUrl: './menu-footer.component.html',
+  styleUrls: ['./menu-footer.component.css']
+})
+export class MenuFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  accionBoton() {
+    console.log("accion boton 1");
+  }
+}
